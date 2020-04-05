6f1763cefdd727b0879653f85df9f7057b9ae2da
kosaka-tatsuma <zlatan0011@gmail.com>
2020-04-05 21:05:39 +0900

    爆誕

---
+++ CodeAnalysis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CodeAnalysis
+{
+	class Program
+	{
+		static Program _Instance = new Program();
+
+		static void Main(string[] args)
+		{
+			var manager = new Buildalyzer.AnalyzerManager(@"C:\Users\Kosaka\Documents\GitHub\BatEn\BatEn.sln");
+
+			foreach(var proj in manager.Projects) {
+				var typedb = new TypeDatabase();
+				typedb.Build(proj.Value);
+
+				foreach(var type_symbol in typedb._TypeSymbolList) {
+					foreach(var member in type_symbol.GetMembers()) {
+						if(member.Kind == SymbolKind.Method && member is IMethodSymbol method_symbol) {
+							Console.WriteLine(method_symbol.ToDisplayString());
+						}
+					}
+				}
+			}
+
+			Console.WriteLine("Fin");
+			Console.ReadKey();
+		}
+
+
+	}
+}
---
+++ CodeAnalysis/TypeDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.CodeAnalysis;
+
+using Buildalyzer;
+using Buildalyzer.Workspaces;
+
+namespace CodeAnalysis
+{
+	public sealed class TypeDatabase
+	{
+		readonly List<ITypeSymbol> _TypeSymbolListInternal = new List<ITypeSymbol>();
+
+		public readonly ReadOnlyCollection<ITypeSymbol> _TypeSymbolList;
+
+		public TypeDatabase()
+		{
+			_TypeSymbolList = new ReadOnlyCollection<ITypeSymbol>(_TypeSymbolListInternal);
+		}
+
+		public void Build(ProjectAnalyzer analyzer)
+		{
+			using(var ws = analyzer.GetWorkspace()) {
+				foreach(var proj in ws.CurrentSolution.Projects) {
+					var comp = proj.GetCompilationAsync().Result;
+					foreach(var cls_symbol in comp.GetSymbolsWithName(_ => true, SymbolFilter.Type).OfType<ITypeSymbol>()) {
+						_TypeSymbolListInternal.Add(cls_symbol);
+					}
+				}
+			}
+
+			foreach(var type_symbol in _TypeSymbolListInternal) {
+				Console.WriteLine(type_symbol.Name);
+				foreach(var member_symbol in type_symbol.GetMembers()) {
+					Console.WriteLine($"\t{member_symbol.Name} - {member_symbol.Kind}");
+				}
+			}
+		}
+	}
+}
